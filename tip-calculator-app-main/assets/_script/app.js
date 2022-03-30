2f3c21df16313102c60be8c86910e83467281c0e
Jorge Ramalhete <jorge.ramalhete@adhesive-studio.com>
2022-03-30 14:40:18 +0100

    update

--- tip-calculator-app-main/assets/_script/app.js
+++ tip-calculator-app-main/assets/_script/app.js
@@ -1,12 +1,12 @@
 let bill_input = document.getElementById('bill_input');
 let number_people_input = document.getElementById('number_people_input');
+let button_calc = document.getElementsByClassName('button-calc');
 let tip_amount = document.getElementById('tip_amount');
 let total_amount = document.getElementById('total_amount');
 let button_reset = document.getElementById('button_reset');
 
 bill_input.addEventListener("change", function(){
     let bill_input_value = document.getElementById('bill_input').value;
-    console.log("Bill = ",bill_input_value);
     if(bill_input_value > 0){
 
         console.log("1 input")
@@ -14,9 +14,16 @@ bill_input.addEventListener("change", function(){
     }
 });
 
+for(var i = 0; i < button_calc.length ; i++){
+    button_calc[i].addEventListener("click", function(){    
+        if(button_calc[0]){
+            console.log(button_calc.value);
+        }
+    });
+}
+
 number_people_input.addEventListener("change", function(){
     let number_people_input_value = document.getElementById('number_people_input').value;
-    console.log("People = ",number_people_input_value);
     if(number_people_input_value > 0){
 
         console.log("2 input")
@@ -24,9 +31,9 @@ number_people_input.addEventListener("change", function(){
 });
 
 button_reset.addEventListener("click", function(){
-    tip_amount.innerHTML = "0";
-    total_amount.innerHTML = "0";
-})
-
-
+    tip_amount.innerHTML = "0.00";
+    total_amount.innerHTML = "0.00";
+    bill_input.value = '';
+    number_people_input.value = '';
+});
 
