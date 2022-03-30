9bb4aaac1be0616b5a93c75f2623918c39e97100
Jorge Ramalhete <jorge.ramalhete@adhesive-studio.com>
2022-03-30 13:25:42 +0100

    update

--- tip-calculator-app-main/assets/_script/app.js
+++ tip-calculator-app-main/assets/_script/app.js
@@ -1,2 +1,32 @@
-var bill_input = document.getElementById('bill_input');
+let bill_input = document.getElementById('bill_input');
+let number_people_input = document.getElementById('number_people_input');
+let tip_amount = document.getElementById('tip_amount');
+let total_amount = document.getElementById('total_amount');
+let button_reset = document.getElementById('button_reset');
+
+bill_input.addEventListener("change", function(){
+    let bill_input_value = document.getElementById('bill_input').value;
+    console.log("Bill = ",bill_input_value);
+    if(bill_input_value > 0){
+
+        console.log("1 input")
+        
+    }
+});
+
+number_people_input.addEventListener("change", function(){
+    let number_people_input_value = document.getElementById('number_people_input').value;
+    console.log("People = ",number_people_input_value);
+    if(number_people_input_value > 0){
+
+        console.log("2 input")
+    }
+});
+
+button_reset.addEventListener("click", function(){
+    tip_amount.innerHTML = "0";
+    total_amount.innerHTML = "0";
+})
+
+
 
