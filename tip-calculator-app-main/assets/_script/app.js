1671cda30b8945587d85f5db523934d745178ca0
Jorge Ramalhete <jorge.ramalhete@adhesive-studio.com>
2022-03-31 19:45:21 +0100

    update

--- tip-calculator-app-main/assets/_script/app.js
+++ tip-calculator-app-main/assets/_script/app.js
@@ -1,14 +1,18 @@
 let bill_input = document.getElementById('bill_input');
 let number_people_input = document.getElementById('number_people_input');
 let button_calc = document.getElementsByClassName('button-calc');
+let custom_amount = document.getElementById('custom_amount');
 let tip_amount = document.getElementById('tip_amount');
 let total_amount = document.getElementById('total_amount');
 let button_reset = document.getElementById('button_reset');
 let tip_amount_result;
 let tip_amount_rounded;
 let tip_percent;
+let number_people = 0;
 
 // tip amount = bill * tip %
+
+tipCalculator();
 
 function displayCalc(tip_percent) {
 
@@ -18,36 +22,47 @@ function displayCalc(tip_percent) {
     $("#tip_amount").text(tip_amount_rounded);
 }
 
-$("#bill_input").on("change keyup paste", function () {
+function tipCalculator() {
 
-    if (bill_input.value > 0) {
-        displayCalc(1);
-    }
-})
+    $("#bill_input").on("change keyup paste", function () {
 
-button_calc[0].addEventListener("click", function () {
-    displayCalc(0.05);
-});
+        if (bill_input.value > 0) {
+            displayCalc(1);
+        }
+    });
 
-button_calc[1].addEventListener("click", function () {
-    displayCalc(0.10);
-});
+    $("#number_people_input").on("change keyup paste", function () {
+        if (bill_input.value > 0) {
+            displayCalc(1);
+        } else {
+            console.log("Cant be Zero");
+        }
+    });
 
-button_calc[2].addEventListener("click", function () {
-    displayCalc(0.15);
-});
+    button_calc[0].addEventListener("click", function () {
+        displayCalc(0.05);
+    });
 
-button_calc[3].addEventListener("click", function () {
-    displayCalc(0.25);
-});
+    button_calc[1].addEventListener("click", function () {
+        displayCalc(0.10);
+    });
 
-button_calc[4].addEventListener("click", function () {
-    displayCalc(0.50);
-});
+    button_calc[2].addEventListener("click", function () {
+        displayCalc(0.15);
+    });
 
-button_reset.addEventListener("click", function () {
-    tip_amount.innerHTML = "0.00";
-    total_amount.innerHTML = "0.00";
-    bill_input.value = '';
-    number_people_input.value = '';
-});
+    button_calc[3].addEventListener("click", function () {
+        displayCalc(0.25);
+    });
+
+    button_calc[4].addEventListener("click", function () {
+        displayCalc(0.50);
+    });
+
+    button_reset.addEventListener("click", function () {
+        tip_amount.innerHTML = "0.00";
+        total_amount.innerHTML = "0.00";
+        bill_input.value = '';
+        number_people_input.value = '';
+    });
+}
