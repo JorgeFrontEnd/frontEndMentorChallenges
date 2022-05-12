8debaa8daecb9628ed1d3dfcebc757fb4c9f5acd
Jorge Ramalhete <jorge.ramalhete@adhesive-studio.com>
2022-05-12 20:42:05 +0100

    update

--- space-tourism-website-main/starter-code/assets/dev/_script/nav.js
+++ space-tourism-website-main/starter-code/assets/dev/_script/nav.js
@@ -1,3 +1,13 @@
 $(function(){
     $("#nav-placeholder").load("nav.html");
 });
+
+// document.querySelectorAll(".nav-link").addEventListener('click', function() {
+//     document.getElementsByClassName("active").classList.remove("active");
+//     this.classList.add("active");
+// });
+
+$('.nav-link').on('click',function(){
+    $('.active').removeClass('active');
+    $(this).addClass('active-link');
+});
